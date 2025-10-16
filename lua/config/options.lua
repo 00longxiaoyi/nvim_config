4d93839645a86b7c10e92c4d8d1fbde6ed8c8997
longxiaoyi <00longxiaoyi@gmail.com>
2025-10-16 13:29:14 +0800

    add: 添加默认终端配置

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 强制使用系统默认终端
+vim.g.terminal_emulator = "konsole" -- 或者 "alacritty", "gnome-terminal" 等你已有的
