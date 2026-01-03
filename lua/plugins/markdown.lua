559337730ebd1f18b16faf8b652f0aa97e2412b0
longxiaoyi <00longxiaoyi@gmail.com>
2026-01-03 20:31:26 +0800

    新增markdown解析插件

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = { enabled = true },
+      code = { enabled = true },
+      latex = { enabled = false },
+    },
+  },
+}
