c5f90d3ec143304ecb6cd08cfbb78d6bf7025d18
longxiaoyi <00longxiaoyi@gmail.com>
2026-01-03 20:32:15 +0800

    新增 前端 tailwind 高亮插件

---
+++ lua/plugins/tailwind-highlight.lua
@@ -0,0 +1,9 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  opts = {
+    filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact" },
+    user_default_options = {
+      tailwind = true,
+    },
+  },
+}
