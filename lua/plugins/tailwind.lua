3c7b6029067e184f29228b5a32e0409a1988060e
longxiaoyi <00longxiaoyi@gmail.com>
2026-01-03 20:33:07 +0800

    新增 tailwind 集成插件

---
+++ lua/plugins/tailwind.lua
@@ -0,0 +1,24 @@
+return {
+  -- 启用 TailwindCSS LSP
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          filetypes = {
+            "html",
+            "css",
+            "scss",
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "vue",
+            "svelte",
+            "astro",
+          },
+        },
+      },
+    },
+  },
+}
