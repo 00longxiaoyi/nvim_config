112fcf759d90fbe12ba6647b51ffe9242d42f4d1
longxiaoyi <00longxiaoyi@gmail.com>
2026-01-03 20:32:50 +0800

    新增 tailwind 提示插件

---
+++ lua/plugins/tailwind-tools.lua
@@ -0,0 +1,17 @@
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    document_color = {
+      enabled = true,
+      kind = "inline",
+    },
+    conceal = {
+      enabled = false,
+    },
+    format = {
+      enabled = true,
+      style = "prettier",
+    },
+  },
+}
